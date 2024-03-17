97c188606d39ce6214aaec11fee2c5b9ff7c49ba
Kenneth <kenneth@radunz.ch>
2024-03-17 16:51:22 +0100

    Add EmptyObject

---
+++ packages/common/test/types/empty-object.spec.ts
@@ -0,0 +1,20 @@
+import { expectType, TypeOf } from 'ts-expect'
+import { EmptyObject } from '../../src'
+
+class EmptyClass {}
+
+describe('EmptyObject', () => {
+  it('should only be assignable from an object with no properties', () => {
+    expectType<EmptyObject>({})
+  })
+
+  it('should not be assignable from any other value', () => {
+    expectType<TypeOf<EmptyObject, EmptyClass>>(false)
+    expectType<TypeOf<EmptyObject, any[]>>(false)
+    expectType<TypeOf<EmptyObject, null>>(false)
+    expectType<TypeOf<EmptyObject, undefined>>(false)
+    expectType<TypeOf<EmptyObject, string>>(false)
+    expectType<TypeOf<EmptyObject, number>>(false)
+    expectType<TypeOf<EmptyObject, () => void>>(false)
+  })
+})
