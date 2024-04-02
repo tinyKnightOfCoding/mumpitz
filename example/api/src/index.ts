f20102175f321ed97bdf9cfeb07897a124bde059
Kenneth <kenneth@radunz.ch>
2024-04-02 11:51:44 +0200

    clear api

--- example/api/src/index.ts
+++ example/api/src/index.ts
@@ -1,13 +1,1 @@
-import { ZodStruct, ZodValue } from '@mumpitz/plugin-zod'
-import { z } from 'zod'
-import { uuidv7 } from 'uuidv7'
-
-export class TodoId extends ZodValue(z.string().uuid()) {
-  static random() {
-    return new TodoId(uuidv7())
-  }
-}
-
-export class TodoDto extends ZodStruct({
-  id: TodoId.prop().default(() => uuidv7()),
-}) {}
+console.log('Hello Api')
